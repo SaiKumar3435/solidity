986f86cbf9650702a092f24a659f756590f3d45b
SaiKumar3435 <117637964+SaiKumar3435@users.noreply.github.com>
2023-03-01 20:53:31 +0530

    Create counter.sol

---
+++ counter.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint256 count;
+
+    function getCount() public view returns (uint256) {
+        return count;
+    }
+
+    function incrementCount() public {
+        count++;
+    }
+}
